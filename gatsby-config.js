e48bc86f0d04f853ffd688e894ae15a0a7f5fd19
mikawang0613 <38368796+mikawang0613@users.noreply.github.com>
2022-08-25 11:48:12 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         facebookPixel: {
-          pixelId: "1111111",
+          pixelId: "840436490666660",
           cookieName: "cookie-consent",
         },
         environments: ["production", "development"],
